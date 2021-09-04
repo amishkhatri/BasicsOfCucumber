ae1f106b6e9a92f2b2da75c08d1b82524c2cc077
amish.khatri <amish.khatri@gmail.com>
2021-09-04 10:16:38 +0530

    Initial commit

---
+++ src/test/java/basic/steps/LoginSteps.java
@@ -0,0 +1,27 @@
+package basic.steps;
+
+import io.cucumber.java.en.*;
+import org.junit.*;
+
+public class LoginSteps {
+	@Given("I am on {string}")
+	public void i_am_on(String url) {
+		System.out.println("I navigate to: " + url);
+	}
+
+	@When("I enter username as {string} and password as {string}")
+	public void i_enter_username_as_and_password_as(String username, String password) {
+		System.out.println("I enter username as: " + username);
+		System.out.println("I enter password as: " + password);
+	}
+
+	@When("I click on login button")
+	public void i_click_on_login_button() {
+		System.out.println("I click on login button");
+	}
+
+	@Then("I should see {string}")
+	public void i_should_see(String message) {
+		Assert.assertEquals(message, "Welcome Tin Tin");
+	}
+}
